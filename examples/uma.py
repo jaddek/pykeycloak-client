a54be839521a8013c216f3b8d8dc5f345cf0f1d7
tonynazarov <tonynazarov@gmail.com>
2026-04-14 07:24:56 +1200

    - fixes minor

--- examples/uma.py
+++ examples/uma.py
@@ -15,6 +15,17 @@ async def main():
         payload=get_user_credentials()
     )
 
+    res = await keycloak.uma.get_uma_permissions_async(
+        payload=UMAAuthorizationPayload(
+            audience=None,
+            permissions=["/otago/roles#update", "/otago/users#update"],
+            subject_token=result.access_token,
+            response_mode=UrnIetfOauthUmaTicketResponseModeEnum.PERMISSIONS,
+        )
+    )
+
+    print(res)
+
     res = await keycloak.uma.get_permissions_by_uris_chunks_async(
         payload=UMAAuthorizationPayload(
             audience=None,
